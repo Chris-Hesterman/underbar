1a886bd84ba4a2c479dc2afa8ecaa3754457e12e
Chris Hesterman <spoiler14@comcast.net>
2020-01-05 11:23:22 -0800

    Remove extra whitespace from _.contains code, underbar.js

--- src/underbar.js
+++ src/underbar.js
@@ -211,11 +211,9 @@
     }
 
     return _.reduce(collection, function(wasFound, item) {
-
       if (wasFound) {
         return true;
       }
-
       return item === target;
     }, false);
   };
